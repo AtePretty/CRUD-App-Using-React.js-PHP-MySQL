d8b747d12fa9ee5da10816617abe08c8ac43abee
Alger Tita Jean L. Bajo <algertitajeanbajo@gmail.com>
2023-01-11 13:00:09 +0800

    Create a route to all my components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+
+import CreateDining from './components/create/CreateDining';
+import CreateMenu from './components/create/CreateMenu';
+import CreateFood from './components/create/CreateFood';
+
+import UpdateDining from './components/update/UpdateDining';
+import UpdateMenu from './components/update/UpdateMenu';
+import UpdateFood from './components/update/UpdateFood';
+
+import ReadDiningMenu from './components/read/ReadDiningMenu';
+
+import Admin from './components/Admin';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/user' element={<ReadDiningMenu/>}/>
+        <Route path='/admin' element={<Admin/>}/>
+        <Route path='/admin-add-dining' element={<CreateDining/>}/>
+        <Route path='/admin-edit-dining' element={<UpdateDining/>}/>
+        <Route path='/admin-add-menu' element={<CreateMenu/>}/>
+        <Route path='/admin-edit-menu' element={<UpdateMenu/>}/>
+        <Route path='/admin-add-food' element={<CreateFood/>}/>
+        <Route path='/admin-edit-food' element={<UpdateFood/>}/>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
