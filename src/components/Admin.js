e420c697fcd20203c658b7be7f9b77590c256094
Alger Tita Jean L. Bajo <algertitajeanbajo@gmail.com>
2023-01-11 13:29:55 +0800

    This should only be accessible by the admin

---
+++ src/components/Admin.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Admin() {
+  return (
+    <div>
+      <p><Link to='/'>Go back</Link></p>
+      <p><Link to='/admin-add-dining'>Add More Dining Name</Link></p>
+      <p><Link to='/admin-add-menu'>Add More Menu</Link></p>
+      <p><Link to='/admin-add-food'>Add More Food</Link></p>
+    </div>
+  )
+}
+
+export default Admin
