2b26030d935f90fc0bd4f5bc7d17a95b65f8acd5
Alger Tita Jean L. Bajo <algertitajeanbajo@gmail.com>
2023-01-12 09:47:23 +0800

    Filter by dining category is now working

--- src/components/read/ReadDiningMenu.js
+++ src/components/read/ReadDiningMenu.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import { Link } from 'react-router-dom'
+import './ReadDiningMenu.css'
 
 function ReadDiningMenu() {
     const [diningNames, setDiningNames] = useState([])
@@ -18,10 +19,12 @@ function ReadDiningMenu() {
     }, [])
 
     const [menus, setMenus] = useState([])
+    const [filter, setFilter] = useState([])
     async function getAllMenuNames() {
         try {
             const response = await Axios.get("http://localhost/Capstone/API/API_Dining_class.php")
             setMenus(response.data.dining)
+            setFilter(response.data.dining)
         }
         catch (error) {
             console.error(error)
@@ -31,18 +34,32 @@ function ReadDiningMenu() {
         getAllMenuNames()
     }, [])
 
+    const [active, setActive] = useState("0")
+    function filterMenus() {
+      if(active === "0") {
+        setFilter(menus)
+        return
+      }
+      const filtered = menus.filter((menu) => menu.dining_id === active);
+      setFilter(filtered)
+    }
+    useEffect(() => {
+        filterMenus();
+    }, [active])
+
     return (
         <div>
             <p><Link to='/'>Go back</Link></p>
             <h1>Dining Page</h1>
+            <button className={active === "0" ? "active" : ""} onClick={() => setActive("0")}>All</button>
             {diningNames.map((diningName) => {
                 return (
-                    <button key={diningName.id} className={diningName.name}>{diningName.name}</button>
+                    <button key={diningName.id} className={active === diningName.id ? "active" : ""} onClick={() => setActive(diningName.id)}>{diningName.name}</button>
                 )
             })}
-            {menus.map((menu, index) => {
+            {filter.map((menu, index) => {
                 return (
-                    <div key={menu.id} id="menu-cards">
+                    <div key={menu.id} className={active === menu.dining ? "active" : ""} onClick={() => setActive(menu.dining)}>
                         <div>
                             <img src={window.location.origin + '/uploads/' + menu.img} alt={menu.menu}/>
                         </div>
