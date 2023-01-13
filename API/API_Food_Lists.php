9074dedfbf9950517027e8e98f5d72f460656aad
Alger Tita Jean L. Bajo <algertitajeanbajo@gmail.com>
2023-01-14 02:02:25 +0800

    API for the list of foods

---
+++ API/API_Food_Lists.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-Type: application/json");
+include_once("../Connection.php");
+$con = connection();
+
+$sql = "SELECT * FROM food_class INNER JOIN menu_class ON food_class.menu_id = menu_class.id";
+$result = $con->query($sql) or die($con->error);
+$row = $result->fetch_assoc();
+
+$food = array();
+
+do {
+    $itemData = new stdClass();
+    $itemData->id = $row["id"];
+    $itemData->name = $row["food_name"];
+    $itemData->menu_id = $row["menu_id"];
+    $itemData->menu_name = $row["menu_name"];
+    array_push($food, $itemData);
+} while($row = $result->fetch_assoc());
+
+$total = count($food);
+
+$foodClass = new stdClass();
+$foodClass->total = $total;
+$foodClass->food = $food;
+
+echo json_encode($foodClass);
+exit;
+?>
