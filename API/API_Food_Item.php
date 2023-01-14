894ff36ae2802022220771c9e207ae2436aed516
Alger Tita Jean L. Bajo <algertitajeanbajo@gmail.com>
2023-01-14 15:41:09 +0800

    add the image name

--- API/API_Food_Item.php
+++ API/API_Food_Item.php
@@ -3,7 +3,7 @@ header("Content-Type: application/json");
 include_once("../Connection.php");
 $con = connection();
 
-$sql = "SELECT item_class.id, item_class.item_name, item_class.food_id, food_class.food_name FROM item_class INNER JOIN food_class ON item_class.food_id = food_class.id";
+$sql = "SELECT item_class.id, item_class.item_name, item_class.item_img, item_class.food_id, food_class.food_name FROM item_class INNER JOIN food_class ON item_class.food_id = food_class.id";
 $result = $con->query($sql) or die($con->error);
 $row = $result->fetch_assoc();
 
@@ -13,6 +13,7 @@ do {
     $itemData = new stdClass();
     $itemData->id = $row["id"];
     $itemData->name = $row["item_name"];
+    $itemData->img = $row["item_img"];
     $itemData->food_id = $row["food_id"];
     $itemData->food_name = $row["food_name"];
     array_push($item, $itemData);
