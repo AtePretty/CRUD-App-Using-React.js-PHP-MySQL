6bba88dddda3c0c3f4e0d8dc2ad6e93e6489ec8a
Alger Tita Jean L. Bajo <algertitajeanbajo@gmail.com>
2023-01-14 15:44:32 +0800

    add styles

--- src/components/create/CreateItem.js
+++ src/components/create/CreateItem.js
@@ -33,6 +33,7 @@ function CreateItem() {
             setItemName('')
             setItemImg('')
             setFoodId('')
+            window.location.href = '/admin';
         })
         .catch(error => console.error(error));
     }
@@ -53,29 +54,31 @@ function CreateItem() {
 
 
     return (
-        <div>
+        <div className='general_form up_menu'>
             <p><Link to='/admin'>Go back</Link></p>
-            <form action="" method="post" encType="multipart/form-data">
-                <label style={{ margin: '0 0 10px', display: 'block' }}>
+            <form action="" method="post" encType="multipart/form-data" style={{width: '500px'}}>
+                <label>
                     Food Item Name
-                    <input ref={item_name} style={{width: '100%'}} type="text" id="item_name" name="item_name" onChange={(e) => setItemName(e.target.value)} />
+                    <input ref={item_name} type="text" id="item_name" name="item_name" onChange={(e) => setItemName(e.target.value)} />
                 </label>
 
-                <label style={{ margin: '0 0 10px', display: 'block' }}>
+                <label>
                     Image
-                    <input style={{width: '100%'}} type="file" name="item_img" accept="image/png, image/jpeg" ref={item_img} onChange={(e) => setItemImg(e.target.files)} />
+                    <input type="file" name="item_img" accept="image/png, image/jpeg" ref={item_img} onChange={(e) => setItemImg(e.target.files)} />
                 </label>
 
-                <select defaultValue={''} required name='food_id' id='food_id' ref={food_id} onChange={(e) => setFoodId(e.target.value)}>
-                    <option value={''} disabled>Please select</option>
-                    {foods.map((food) => {
-                        return (
-                            <option key={food.id} value={food.id}>{food.name}</option>
-                        )
-                    })}
-                </select>
+                <label>Food Category
+                    <select defaultValue={''} required name='food_id' id='food_id' ref={food_id} onChange={(e) => setFoodId(e.target.value)}>
+                        <option value={''} disabled>Please select</option>
+                        {foods.map((food) => {
+                            return (
+                                <option key={food.id} value={food.id}>{food.name}</option>
+                            )
+                        })}
+                    </select>
+                </label>
 
-                <input style={{padding: '5px 15px'}} type="button" name='send' value="send" onClick={handleSubmit} />
+                <button type="button" name='send' onClick={handleSubmit}>Insert</button>
             </form>
         </div>
     )
