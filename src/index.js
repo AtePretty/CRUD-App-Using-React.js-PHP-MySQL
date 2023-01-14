325e5ed87d2bae2c0f86d661a3f9975eafb3d08e
Alger Tita Jean L. Bajo <algertitajeanbajo@gmail.com>
2023-01-14 14:28:48 +0800

    start the AOS

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+AOS.init()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
