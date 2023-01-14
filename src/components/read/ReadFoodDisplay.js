0880526ff6671607ec7fe38c1e75cf6dc72c3298
Alger Tita Jean L. Bajo <algertitajeanbajo@gmail.com>
2023-01-14 16:07:30 +0800

    displays the items by its food and menu
    there is no style for this one, need to style it first

--- src/components/read/ReadFoodDisplay.js
+++ src/components/read/ReadFoodDisplay.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import { Link, useLocation } from 'react-router-dom'
 import './ReadDiningMenu.css'
+import Hero from '../jsx/Hero'
+import './ReadFoodDisplay.css'
 
 function ReadFoodDisplay() {
     const location = useLocation()
@@ -23,38 +25,50 @@ function ReadFoodDisplay() {
     }, [])
 
     return (
-        <div>
-            <p><Link to='/user'>Go back</Link></p>
+        <div className='dining_page food_menu'>
+        <div className='width_allowed'>
+            <Hero/>
             {menus.map((menu) => {
                 if (menu.menu_id === menu_pass) {
                     return (
-                        <div key={menu.menu_id}>
-                            <h1>Menu Name: {menu.menu_name}</h1>
-                            <p>Description: {menu.menu_desc}</p>
+                        <div className='mainNiMenu' key={menu.menu_id}>
+                            <p className='backLink'><Link to='/user'>Go back</Link></p>
+                            <h1>{menu.menu_name}</h1>
+                            <div className='separation'>
+                            <div  className='description'>
+                                <p>{menu.menu_desc}</p>
+                            </div>
+                            <div>
                             {menu.menu_food.map((food) => {
                                 return (
-                                    <div key={food.food_id}>
+                                    <div className='MenuList' key={food.food_id}>
                                         <h2>{food.food_name}</h2>
+                                        <div className='imageLahat'>
                                         {food.food_item.map((item) => {
                                             return (
                                                 <div key={item.item_id}>
                                                 {/* 
-                                                    This is connected to the bug I found in the API_Fodd_class.php,
+                                                    This is connected to the bug I found in the API_Food_class.php,
                                                     the data in $itemArray is duplicated. I still haven't found the
                                                     bug yet.
                                                 */}
-                                                    <img src={window.location.origin + '/uploads/' + item.item_img} alt="item.item_name" />
+                                                <div className='itemImage'>
+                                                    <img src={window.location.origin + '/uploads/' + item.item_img} alt="item.item_name" /></div>
                                                     <p>{item.item_name}</p>
                                                 </div>
                                             )
                                         })}
+                                        </div>
                                     </div>
                                 )
                             })}
+                            </div>
+                            </div>
                         </div>
                     ) 
                 }
             })}
+        </div>
         </div>
     )
 }
